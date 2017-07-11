b6488331ba264ee27dddbee2e0af8a380e5aca79
Ktlo <ktlopost@hotmail.com>
2017-07-11 19:58:22 +0300

    Added "config.h" file

---
+++ src/config.h
@@ -0,0 +1,33 @@
+/**********************************************************\
+| Project: "ManagedLua.dll"                                |
+| File: "config.h"                                         |
+|##########################################################|
+|   Contains preprocessor derectives, libraries to include |
+| and disables some VisualC++ compile errors.              |
+|##########################################################|
+| Author: Ktlo                                             |
+| Date: 04.03.2017 23:32                                   |
+\**********************************************************/
+
+#ifndef __LIB_CONFIG
+#define __LIB_CONFIG
+
+#pragma warning( disable : 4996 )
+#pragma warning( disable : 4302 )
+#pragma warning( disable : 4244 )
+#pragma comment( lib, "lua53.lib" )
+
+//The main and absolute namespace.
+#define BRIGHT_CUBE_BEGIN namespace bright_cube {
+#define BRIGHT_CUBE_END }
+
+//Exports
+#define CLRLUA_LIB __declspec( dllexport )
+
+//For maybe-inline methods
+#define INLINE inline
+
+//COM Invisible
+#define HIDE [ComVisible(false)]
+
+#endif // __LIB_CONFIG
